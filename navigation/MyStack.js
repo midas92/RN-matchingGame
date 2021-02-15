fef915c3330b47d1461273309a0bbc99fea8dc4d
midas92 <dounanpar@gmail.com>
2021-02-15 17:22:02 -0500

    removed unused imports

--- navigation/MyStack.js
+++ navigation/MyStack.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator, HeaderBackButton } from '@react-navigation/stack';
+import { createStackNavigator } from '@react-navigation/stack';
 
 import HomeScreen from "../screens/HomeScreen";
 import SetupGameScreen from "../screens/SetupGameScreen";
